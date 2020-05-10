2afda76bdbacb4731045e7bca8430c97264bd9fe
Gonzalo Salinas <gsalinase91@gmail.com>
2020-05-10 12:00:17 -0400

    First Commit

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -62,13 +62,13 @@ class StoriesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_story
-      @story = Story.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_story
+    @story = Story.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def story_params
-      params.require(:story).permit(:title, :picture, :content)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def story_params
+    params.require(:story).permit(:title, :picture, :content)
+  end
 end
